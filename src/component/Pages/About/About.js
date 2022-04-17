8a7fc458f949c942fa05af9a6e73d4ffa4591712
PalashDas11 <pnpalash20@gmail.com>
2022-04-17 14:33:15 +0600

    blog page and about page

--- src/component/Pages/About/About.js
+++ src/component/Pages/About/About.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './About.css'
+import AboutImg from '../../../img/About.jpg'
 
 const About = () => {
     return (
-        <div>
-            <h2>this is about page</h2>
+        <div className='row container align-items-center justify-content-around p-4'>
+            <div className='col-sm-12 col-md-6 col-lg-4 about-img'>
+                <img src={AboutImg} alt="" />
+            </div>
+            <div className='col-sm-12 col-md-6 col-lg-8' >
+                <h4>About me-</h4>
+                <p>Hi, Welcome to About page- My name is Palash Das, I'm a Front-end developer. I can bulid your website using React, React-Router firebase. I have more experices HTML, CSS, JavaScript. And my goal is will be a Full-Stack Developer.....Thank for reading!</p>
+            </div>
         </div>
     );
 };
