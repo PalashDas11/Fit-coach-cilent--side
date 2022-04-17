c42721fd95a7d0070e9468e20ed6584df932211f
PalashDas11 <pnpalash20@gmail.com>
2022-04-18 02:46:40 +0600

    google signin

--- src/component/Pages/SocialLogin/SocialLogin.js
+++ src/component/Pages/SocialLogin/SocialLogin.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import google from '../../../img/google.png'
+import Load from '../Load/Load';
 
 const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+    let errorMessage;
+
+    if(loading ){
+        return <Load></Load>
+    }
+
+    if (error) {
+        errorMessage = <p className='text-danger'>Error: {error?.message}</p>
+    }
+
+    if (user) {
+        navigate('/home');
+    }
+
+
     return (
         <div>
             <div className='d-flex align-items-center'>
@@ -9,8 +30,9 @@ const SocialLogin = () => {
                 <p className='mt-2 px-2'>or</p>
                 <div style={{ height: '1px' }} className='custom-bg-primary w-50'></div>
             </div>
+            {errorMessage}
             <button
-                    // onClick={() => signInWithGoogle()}
+                    onClick={() => signInWithGoogle()}
                     className='btn custom-bg-primary d-block mx-auto my-2'>
                     <img style={{ width: '30px' }} src={google} alt="" />
                     <span className=' text-white'>Google Sign In</span>
