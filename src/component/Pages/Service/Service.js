8fed759e5b467fd348fa7b8d0dbd08ddc4dca92d
PalashDas11 <pnpalash20@gmail.com>
2022-04-17 16:37:49 +0600

    services-section

--- src/component/Pages/Service/Service.js
+++ src/component/Pages/Service/Service.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-const Service = ({service}) => {
-   
+const Service = ({ service }) => {
+    const { name, title, img, price } = service
     console.log(service);
     return (
-        <div>
-            
+        <div className='col-sm-12 col-md-3 d-block mx-auto'>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={img} />
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text>
+                        {title}
+                    </Card.Text>
+                    <div className='d-flex align-items-center justify-content-between'>
+                        <h5>${price}<small>/month</small></h5>
+                        <Link to="/login" className='custom-btn'>Enroll Now</Link>
+                    </div>
+                </Card.Body>
+            </Card>
         </div>
     );
 };
--- src/component/Pages/Services/Services.js
+++ src/component/Pages/Services/Services.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import Service from '../Service/Service';
 const services = [
-    {id : 1, name:'Core Strength', title:'kdfi lwi emlwie',price:15,img:'https://fitnesszonedev.wpengine.com/wp-content/uploads/2015/02/blog6.jpg'},
-    {id : 2, name:'Muscle Bulding',title:' difd psjd osj ao',price:25,img:'https://fitnesszonedev.wpengine.com/wp-content/uploads/2015/02/product8-420x287.jpg'},
-    {id : 3, name:'All Round Fitness',title:'dfi osij oij sikj',price:15,img:'https://fitnesszonedev.wpengine.com/wp-content/uploads/2015/02/blog2.jpg'},
+    {id : 1, name:'Core Strength', title:'It may seem like a no-brainer, but doing core exercises builds core strength. Its hard to strengthen a muscle group without doing exercises to strengthen it. Core exercises dont just help tone and sculpt six-pack abs! Core exercises are great for improving overall balance',price:20,img:'https://fitnesszonedev.wpengine.com/wp-content/uploads/2015/02/blog6.jpg'},
+    {id : 2, name:'Muscle Bulding',title:'You have probably heard that you should be incorporating strength training into your exercise routine..Spending your whole day in the gym is not necessary to build muscle. Weight training for 20 to 30 minutes, 2 to 3 times a week is enough to see results. For muscle build you need to eat better.',price:25,img:'https://fitnesszonedev.wpengine.com/wp-content/uploads/2015/02/product8-420x287.jpg'},
+    {id : 3, name:'Fat Burning Pro',title:'Everyone has a different reason for working out. Some want to accomplish performance goals, others want to build enough muscle to make their shirtsleeves bulge. But on some level, just about everyone trains to be healthier—and that means they’re working up a sweat and burning through fat.',price:15,img:'https://fitnesszonedev.wpengine.com/wp-content/uploads/2015/02/blog2.jpg'},
 ]
 const Services = () => {
     return (
-        <div>
+        <div className='py-4'>
+            <div>
+                <h3 className='text-center'>Services</h3>
+            </div>
+        <div className='row container p-4'>
             {
                 services.map(service =><Service key={service.id} service = {service}></Service>)
             }
+        </div>
         </div>
     );
 };
